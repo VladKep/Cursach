16302bf31eab61333b7a41c9dcbdd24a97ad50c6
Illia Kamarali <illia.kamarali.work@gmail.com>
2023-11-29 20:39:22 +0100

    front-end changes with reg and log

--- src/main/java/com/example/cursach/Config/SecurityConfig.java
+++ src/main/java/com/example/cursach/Config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -32,7 +33,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
                 .defaultSuccessUrl("/", true)
-                .failureForwardUrl("/auth/login?error");
+                .failureForwardUrl("/auth/login?error")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/login");
     }
 
     @Bean
--- src/main/java/com/example/cursach/Service/RegistrationService.java
+++ src/main/java/com/example/cursach/Service/RegistrationService.java
@@ -3,19 +3,24 @@ package com.example.cursach.Service;
 import com.example.cursach.Model.Client;
 import com.example.cursach.Repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RegistrationService {
     private final ClientRepository clientRepository;
+    // private final PasswordEncoder passwordEncoder;
     @Autowired
-    public RegistrationService(ClientRepository clientRepository) {
+    public RegistrationService(ClientRepository clientRepository, PasswordEncoder passwordEncoder) {
         this.clientRepository = clientRepository;
+        // this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void registration(Client client) {
+//        String encodedPassword = passwordEncoder.encode(client.getPassword());
+//        client.setPassword(encodedPassword);
         clientRepository.save(client);
     }
 }
