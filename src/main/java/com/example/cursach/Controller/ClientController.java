bb889973b4b5c055c21f01c3ad11507984b13dd7
Illia Kamarali <illia.kamarali.work@gmail.com>
2023-12-04 23:47:52 +0100

    changes client's info

--- src/main/java/com/example/cursach/Controller/ClientController.java
+++ src/main/java/com/example/cursach/Controller/ClientController.java
@@ -1,22 +1,24 @@
 package com.example.cursach.Controller;
 
-import com.example.cursach.Service.ClientServiceAdmin;
-import com.example.cursach.Service.ParkingSpotService;
+import com.example.cursach.Model.Client;
+import com.example.cursach.Service.ClientService;
 import com.example.cursach.Security.ClientDetails;
+import com.example.cursach.Service.ClientServiceAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ClientController {
     @Autowired
-    private ParkingSpotService parkingSpotService;
+    private ClientService clientService;
+    private ClientServiceAdmin clientServiceAdmin;
 
     @GetMapping("")
     public String mainPage() {
@@ -25,15 +27,27 @@ public class ClientController {
 
     @GetMapping("/available")
     public String availableSpots(Model model) {
-        System.out.println(parkingSpotService.availableParkingSpots());
-        model.addAttribute("available", parkingSpotService.availableParkingSpots());
+        System.out.println(clientService.availableParkingSpots());
+        model.addAttribute("available", clientService.availableParkingSpots());
         return "client/freeSpots";
     }
 
     @GetMapping("/my-info")
     public String userInfo(@AuthenticationPrincipal ClientDetails clientDetails, Model model) {
-        model.addAttribute("client", clientDetails);
+        model.addAttribute("client", clientDetails.getClient());
         return "client/clientInfo";
+    }
+
+    @GetMapping("/my-info/edit")
+    public String edit(@AuthenticationPrincipal ClientDetails clientDetails, Model model) {
+        model.addAttribute("client", clientDetails.getClient());
+        return "client/clientInfoEdit";
+    }
+
+    @PostMapping("/{id}")
+    public String update(@ModelAttribute("client")Client client, @PathVariable("id") int id) {
+        clientService.update(id, client);
+        return "redirect:/my-info";
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
--- src/main/java/com/example/cursach/Service/ClientService.java
+++ src/main/java/com/example/cursach/Service/ClientService.java
@@ -3,25 +3,45 @@ package com.example.cursach.Service;
 import com.example.cursach.Model.Client;
 import com.example.cursach.Model.Note;
 import com.example.cursach.Model.ParkingSpot;
+import com.example.cursach.Repository.ClientRepository;
 import com.example.cursach.Repository.NoteRepository;
 import com.example.cursach.Repository.ParkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ClientService {
     private final ParkingSpotRepository parkingSpotRepository;
     private final NoteRepository noteRepository;
+    private final ClientRepository clientRepository;
     @Autowired
-    public ClientService(ParkingSpotRepository parkingSpotRepository, NoteRepository noteRepository) {
+    public ClientService(ParkingSpotRepository parkingSpotRepository, NoteRepository noteRepository, ClientRepository clientRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
         this.noteRepository = noteRepository;
+        this.clientRepository = clientRepository;
     }
 
+    @Transactional
     public void reservation(LocalDateTime start, LocalDateTime end, Client client, ParkingSpot parkingSpot) {
         Note note = new Note(start, end, client, parkingSpot);
         noteRepository.save(note);
+    }
+
+    public List<ParkingSpot> availableParkingSpots() {
+        return parkingSpotRepository.findParkingSpotsByStatus("Вільно");
+    }
+
+    @Transactional
+    public void update(int id, Client client) {
+        Client client1 = clientRepository.findById(id).orElseThrow();
+        client1.setUsername(client.getUsername());
+        client1.setPhoneNumber(client.getPhoneNumber());
+        client1.setEmail(client.getEmail());
+        client1.setPassword(client.getPassword());
+        clientRepository.save(client1);
     }
 }
