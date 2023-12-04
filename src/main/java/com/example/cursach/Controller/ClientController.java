590ac4f4fb953d244795e364907f960a8a43b731
Illia Kamarali <illia.kamarali.work@gmail.com>
2023-12-04 11:41:58 +0100

    info about client

--- src/main/java/com/example/cursach/Controller/ClientController.java
+++ src/main/java/com/example/cursach/Controller/ClientController.java
@@ -1,14 +1,17 @@
 package com.example.cursach.Controller;
 
+import com.example.cursach.Service.ClientServiceAdmin;
 import com.example.cursach.Service.ParkingSpotService;
 import com.example.cursach.Security.ClientDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ClientController {
@@ -24,15 +27,13 @@ public class ClientController {
     public String availableSpots(Model model) {
         System.out.println(parkingSpotService.availableParkingSpots());
         model.addAttribute("available", parkingSpotService.availableParkingSpots());
-        return "client/hello";
+        return "client/freeSpots";
     }
 
-    @GetMapping("/showUserInfo")
-    public String showInfo() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        ClientDetails clientDetails = (ClientDetails) authentication.getPrincipal();
-        System.out.println(clientDetails.getClient());
-        return "client/hello";
+    @GetMapping("/my-info")
+    public String userInfo(@AuthenticationPrincipal ClientDetails clientDetails, Model model) {
+        model.addAttribute("client", clientDetails);
+        return "client/clientInfo";
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
